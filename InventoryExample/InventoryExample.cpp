3e1b4570458e6205fe9b811d38e49456b6ce0c2c
Вадим <107673843+VadimAbergard@users.noreply.github.com>
2024-06-05 09:06:29 +0300

    Update InventoryExample.cpp

--- InventoryExample/InventoryExample.cpp
+++ InventoryExample/InventoryExample.cpp
@@ -6,18 +6,13 @@
 
 int main()
 {
-    Inventory inv;
+    Inventory inv; // create inventory
 
-    int count = 3;
-    int count2 = 7;
-    inv.setItem(0, Item(ItemType::apple, 3));
-    inv.addItemInSlot(9, Item(ItemType::apple), count2);
-    //bool iRemove = inv.removeItem(Item(ItemType::apple), 6);
-    //int count2 = 1;
-    //inv.addItem(Item(ItemType::stone), count2);
+    int count = 7; // return result from method addItemInSlot
+    inv.setItem(0, Item(ItemType::apple, 3)); 
+    inv.addItemInSlot(9, Item(ItemType::apple), count);
 
-    cout << "= " << count2 << endl;
-    //cout << "isremove = " << iRemove << endl;
+    cout << "= " << count << endl; // we look at how many items are left in the inventory when we put them there
 
-    inv.printConsole();
+    inv.printConsole(); // we display the inventory in the console
 }
